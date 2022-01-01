c68113b410c6b10763b603edf205d7d43fa8f5f8
SeanShin0821 <cshin4@gmu.edu>
2022-01-01 18:08:46 -0500

    script update

---
+++ assets/js/script.js
@@ -0,0 +1,109 @@
+// Universal Variables
+var len = 0;
+var params = [];
+var tmp = '';
+var finalKey = [];
+
+// Object that holds the kes
+var keys = {
+  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+  number: '0123456789',
+  symbol: '!@#$%^&*()_+~\\`|}{[]:;?><,./-=',
+};
+
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(len, getKeys(params));
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+
+  // Resets values for new check;
+  tmp = '';
+  params = [];
+  len = 0;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', function () {
+  // Resets parameters if no selection was made
+  params = [];
+  // Asks useer for lenght of password
+  len = parseInt(
+    window.prompt('Enter password length between 8 and 128 chars.')
+  );
+  // Conditional checks for appropiate format.
+  if (!len) {
+    window.alert('This needs to be a number.');
+    return;
+  } else if (len < 8 || len > 128) {
+    len = window.alert('You need to provide a value between 8 and 128.');
+    return;
+  } else {
+    // Checks the parameters
+    var upperCase = window.confirm('Include upper case?');
+    params.push(upperCase);
+
+    var lowerCase = window.confirm('Include lower case?');
+    params.push(lowerCase);
+
+    var numbers = window.confirm('Include numbers?');
+    params.push(numbers);
+
+    var symbols = window.confirm('Include symbols?');
+    params.push(symbols);
+  }
+  // Conditional to check if at least 1 parameter was selected.
+  if (!upperCase && !lowerCase && !numbers && !symbols) {
+    window.alert('You need to select at least one');
+    return;
+  } else {
+    // If all conditionals pass it wirtes pw to screen
+    writePassword();
+  }
+});
+
+// Function that generates the password
+function generatePassword(len, key) {
+  // Loops while the tmp pw is less than the length povided
+  while (len > tmp.length) {
+    // Picks a random function from the key array
+    var addKey = key[Math.floor(Math.random() * key.length)];
+    tmp += addKey();
+  }
+  return tmp;
+}
+
+// Function to select which keys to use
+function getKeys(params) {
+  // Array with all the functions to randomize characters
+  var getKey = [
+    function upperCase() {
+      return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+    },
+    function lowerCase() {
+      return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+    },
+    function number() {
+      return keys.number[Math.floor(Math.random() * keys.number.length)];
+    },
+    function symbol() {
+      return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
+    },
+  ];
+
+  // Resets final keys if app ran again
+  finalKey = [];
+
+  // Loops the params array and selects the corresponding functions
+  for (var i = 0; i < params.length; i++) {
+    if (params[i]) {
+      finalKey.push(getKey[i]);
+    }
+  }
+  return finalKey;
+}
